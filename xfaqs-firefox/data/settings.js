37d98ac130f5bb9ba9b1d13c7319b489d191c9d0
Kraust <secretdragoon@gmail.com>
2014-08-08 06:17:00 -0400

    should check if it works before making changes

--- xfaqs-firefox/data/settings.js
+++ xfaqs-firefox/data/settings.js
@@ -19,7 +19,7 @@ if( localStorage.getItem("modColor") != null) {
 if( localStorage.getItem("vipColor") != null) {
 	var vipColor = localStorage.getItem("vipColor");
 } else {
-	localStorage.setItem("vipColor", );
+	localStorage.setItem("vipColor", "");
 }
 
 if(	localStorage.getItem("sigList") != null ) {
